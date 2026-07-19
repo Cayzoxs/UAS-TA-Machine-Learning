2beeaeb807634c2afec60c27aabc274c0529dd76
Cayzoxs <cahyamuhamad2005@gmail.com>
2026-07-19 18:48:24 +0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,6 @@ import pickle
 import os
 
 def load_components(model_path='models/best_model.pkl', prep_path='models/preprocessing.pkl'):
-    """
-    Fungsi untuk memuat model XGBoost dan objek Preprocessing (Scaler & Encoder).
-    Sangat berguna untuk dipanggil pada file app.py (Streamlit).
-    """
     if not os.path.exists(model_path) or not os.path.exists(prep_path):
         raise FileNotFoundError("Model atau file preprocessing tidak ditemukan. Pastikan proses training sudah dijalankan.")
         
